7bc045957af923ec886dcf05b2393679a4be37f4
Bryan Greig <bryangreig@Bryans-MacBook-Pro.local>
2025-09-15 14:05:21 -0700

    Textarea added for note input

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
 
   function addNote() {
     const id = Date.now().toString();
-    setNotes(prev => [...prev, { id, title: `Untitled ${prev.length + 1}`, body: "Empty note.." }]);
+    setNotes(prev => [...prev, { id, title: `Untitled ${prev.length + 1}`, body: "" }]);
   }
 
   return (
@@ -14,7 +14,12 @@ export default function App() {
       <button onClick={addNote}>New Note</button>
       <ul>{notes.map(n => <li key={n.id}>
         <strong>{n.title}</strong>
-        <p>{n.body}</p>
+        <textarea
+        value={n.body}
+        onChange={(e) => setNotes((prev) =>
+        prev.map((note) =>
+        note.id === n.id ? { ...note, body: e.target.value} : note)
+        )}></textarea>
         <button onClick={() => setNotes(prev => prev.filter(note => note.id !== n.id))}>Delete</button>
         </li>)}</ul>
     </div>
