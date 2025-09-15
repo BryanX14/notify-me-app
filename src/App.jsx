9bd570684c8a9e02cd10821d2ee129a9f47e01e7
Bryan Greig <bryangreig@Bryans-MacBook-Pro.local>
2025-09-15 13:58:58 -0700

    Added Delete button to remove notes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ export default function App() {
 
   function addNote() {
     const id = Date.now().toString();
-    setNotes(prev => [...prev, { id, title: `Untitled ${prev.length + 1}` }]);
+    setNotes(prev => [...prev, { id, title: `Untitled ${prev.length + 1}`, body: "Empty note.." }]);
   }
 
   return (
     <div>
       <h1>Notify Me</h1>
       <button onClick={addNote}>New Note</button>
-      <ul>{notes.map(n => <li key={n.id}>{n.title}</li>)}</ul>
+      <ul>{notes.map(n => <li key={n.id}>
+        <strong>{n.title}</strong>
+        <p>{n.body}</p>
+        <button onClick={() => setNotes(prev => prev.filter(note => note.id !== n.id))}>Delete</button>
+        </li>)}</ul>
     </div>
   );
 }
